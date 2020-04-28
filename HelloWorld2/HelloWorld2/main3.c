7bd1761391812f1256ba5e8920455f940eec0c8a
jwyeyo <jwyeyo@naver.com>
2020-04-28 11:07:08 +0900

    echo back

---
+++ HelloWorld2/HelloWorld2/main3.c
@@ -0,0 +1,43 @@
+#define F_CPU 16000000L
+#include <avr/io.h>
+#include <util/delay.h>
+
+void UART_INIT(void)
+{
+	UCSR0A |= _BV(U2X0);		// 2배속 모드
+	
+	UBRR0H = 0x00;				// 통신 속도(Baud rate) 설정
+	UBRR0L = 207;
+	
+	// 비동기, 8비트 데이터, 패리티 없음, 1비트 정지 비트 모드
+	UCSR0C |= 0x06;
+	UCSR0B |= _BV(RXEN0);					// 송수신 가능
+	UCSR0B |= _BV(TXEN0);
+}
+
+unsigned char UART_receive(void)
+{
+	while( !(UCSR0A & (1<<RXC0)) );			// 데이터 수신 대기
+	return UDR0;							
+}
+
+void UART_transmit(unsigned char data)
+{
+	while( !(UCSR0A & (1<<UDRE0)) );		// 송신 가능 대기
+	UDR0 = data;							// 데이터 전송
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned char data;
+	
+	UART_INIT();
+	while(1)
+	{
+		data = UART_receive();
+		UART_transmit(data);
+	}
+	
+	return 0;
+}
+
